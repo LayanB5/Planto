3507391b7f959512c532500b093a190b8c8277e6
Layan <LayanDeveloper@hotmail.com>
2025-10-19 10:28:03 +0300

    Initial Commit

---
+++ Planto/PlantoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoApp.swift
+//  Planto
+//
+//  Created by Layan Albarrak on 19/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlantoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlantoTests/PlantoTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlantoTests.swift
+//  PlantoTests
+//
+//  Created by Layan Albarrak on 19/10/2025.
+//
+
+import Testing
+@testable import Planto
+
+struct PlantoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
