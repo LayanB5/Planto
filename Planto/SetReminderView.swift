67b9533a5b53a866c6ef985210a42e84a278b675
Layan <LayanDeveloper@hotmail.com>
2025-10-23 11:02:07 +0300

    first verion

---
+++ Planto/SetReminderView.swift
@@ -0,0 +1,18 @@
+//
+//  SetReminderView.swift
+//  Planto
+//
+//  Created by Layan Albarrak on 21/10/2025.
+//
+
+import SwiftUI
+
+struct SetReminderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SetReminderView()
+}
